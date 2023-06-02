7db74553f00dd57fd523b76790f46fa23ab973f3
Daniel Restrepo <danielcgrafico@email.com>
2023-06-02 09:23:56 +0200

    cambios 2

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -1,50 +1,51 @@
-import { createButton } from './Button';
+// import { createButton } from './Button';
+import '../ds/btn.css'
 
 // More on how to set up stories at: https://storybook.js.org/docs/html/writing-stories/introduction
 export default {
   title: 'Example/Button',
   tags: ['autodocs'],
-  render: ({ label, ...args }) => {
+  render: ({ label}) => {
     // You can either use a function to create DOM elements or use a plain html string!
     // return `<div>${label}</div>`;
     return `<div class="storybook-button storybook-button--primary">${label}</div>`;
   },
   argTypes: {
-    backgroundColor: { control: 'color' },
+    // backgroundColor: { control: 'color' },
     label: { control: 'text' },
-    onClick: { action: 'onClick' },
-    primary: { control: 'boolean' },
-    size: {
-      control: { type: 'select' },
-      options: ['small', 'medium', 'large'],
-    },
+    // onClick: { action: 'onClick' },
+    // primary: { control: 'boolean' },
+    // size: {
+    //   control: { type: 'select' },
+    //   options: ['small', 'medium', 'large'],
+    // },
   },
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/html/writing-stories/args
 export const Primary = {
   args: {
-    primary: true,
+    // primary: true,
     label: 'Button',
   },
 };
 
-export const Secondary = {
-  args: {
-    label: 'Button',
-  },
-};
+// export const Secondary = {
+//   args: {
+//     label: 'Button',
+//   },
+// };
 
-export const Large = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
+// export const Large = {
+//   args: {
+//     size: 'large',
+//     label: 'Button',
+//   },
+// };
 
-export const Small = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
-};
+// export const Small = {
+//   args: {
+//     size: 'small',
+//     label: 'Button',
+//   },
+// };
